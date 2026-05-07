a72edbf2ee2a3e7f440076aa82723dc6ad1d8174
RomainMzg <romain.miazga57@gmail.com>
2026-05-07 19:13:40 +0200

    Déploiement à la racine du domaine

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://RomainMiazga.github.io',
+  base: '/', // On met un slash pour dire "à la racine"
+});
