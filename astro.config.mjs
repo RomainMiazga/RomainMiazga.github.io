95842bc66e9e9034453faf633c2d420011789006
RomainMzg <romain.miazga57@gmail.com>
2026-05-08 10:37:28 +0200

    MAJ profil technicien de proximité

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://RomainMiazga.github.io',
-  base: '/', // On met un slash pour dire "à la racine"
+
+  // On met un slash pour dire "à la racine"
+  base: '/',
+
+  integrations: [sitemap()]
 });
