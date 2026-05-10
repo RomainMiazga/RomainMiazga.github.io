caa2577efd91169dac497a8dc4d337cfaf947969
RomainMzg <romain.miazga57@gmail.com>
2026-05-11 00:25:04 +0200

    MAJ de Mon Portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://RomainMiazga.github.io',
+  site: 'https://romainmiazga.com/',
 
   // On met un slash pour dire "à la racine"
   base: '/',
